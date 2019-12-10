8ae5578df670416176d7582900c2fa6a6fc64300
Oleg <bosoleg89@gmail.com>
2019-12-10 23:51:10 +0200

    Updates

---
+++ precache-manifest.80c15ec7cdd9d0fd8e1a02bf535190cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3aa842acf1e0e14f7bbc523ae1e97f",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "18616e047cbd3ad4b4b3",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.f2942c03.chunk.css"
+  },
+  {
+    "revision": "08d60d6658bc1b737853",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.428dc9a1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.428dc9a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18616e047cbd3ad4b4b3",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.79ed3ac1.chunk.js"
+  },
+  {
+    "revision": "d4399fa6f2f46ea5eef9",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.0ec52e7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/oBorschev/goit-react-hw-03-image-finder/precache-manifest.92e25dfa8011471ee44dba3d6e39af27.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.80c15ec7cdd9d0fd8e1a02bf535190cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oBorschev/goit-react-hw-03-image-finder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-03-image-finder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
