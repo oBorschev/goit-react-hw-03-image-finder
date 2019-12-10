5ed0db6bfd6f9de6f03d91d1a378edf0232b8dfc
Oleg <bosoleg89@gmail.com>
2019-12-10 17:22:13 +0200

    Updates

---
+++ precache-manifest.92e25dfa8011471ee44dba3d6e39af27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552aeb2b886751ca326c4d7bec9290be",
+    "url": "/oBorschev/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "18616e047cbd3ad4b4b3",
+    "url": "/oBorschev/goit-react-hw-03-image-finder/static/css/main.f2942c03.chunk.css"
+  },
+  {
+    "revision": "08d60d6658bc1b737853",
+    "url": "/oBorschev/goit-react-hw-03-image-finder/static/js/2.428dc9a1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/oBorschev/goit-react-hw-03-image-finder/static/js/2.428dc9a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18616e047cbd3ad4b4b3",
+    "url": "/oBorschev/goit-react-hw-03-image-finder/static/js/main.79ed3ac1.chunk.js"
+  },
+  {
+    "revision": "f289d5c78db404e981f0",
+    "url": "/oBorschev/goit-react-hw-03-image-finder/static/js/runtime-main.ac6effbe.js"
+  }
+]);
